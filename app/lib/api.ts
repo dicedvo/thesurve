e40ebc8016352df61d9d1366d1729962142c319d
Ned Palacios <npdoesmc@gmail.com>
2025-02-07 22:07:34 +0800

    fix: auth header

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -3,6 +3,6 @@ import { Config } from "@hey-api/client-axios";
 export const apiClientConfig: Config = {
   baseURL: import.meta.env.SSR ? process.env.VITE_API_URL : import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: 'Bearer ' + import.meta.env.SSR ? process.env.VITE_API_TOKEN : import.meta.env.VITE_API_TOKEN,
+    Authorization: `Bearer ${import.meta.env.SSR ? process.env.VITE_API_TOKEN : import.meta.env.VITE_API_TOKEN}`,
   },
 }
